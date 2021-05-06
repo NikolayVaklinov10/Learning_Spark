0cb7ad1f4dbc964a1c05fd37a8c90e40b12eb026
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-06 22:44:28 +0100

    exploring the data

---
+++ src/main/scala/gettingStarted/MnMcount.scala
@@ -0,0 +1,21 @@
+package gettingStarted
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, desc}
+
+object MnMcount extends App {
+
+  val spark = SparkSession.builder()
+    .appName("MnMCount")
+    .master("local[*]")
+    .getOrCreate()
+
+  val mnm = spark.read
+    .option("inferSchema", true)
+    .csv("src/main/scala/resources/chapter2/mnm_dataset.csv")
+
+  mnm.show()
+
+
+
+}
