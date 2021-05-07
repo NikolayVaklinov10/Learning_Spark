e6031033d1ffa03af1c2023f5a69a774bd784d0d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-07 15:37:50 +0100

    the aggregation of MnM colors by US stats by selecting the columns needed, groupBy State and Color and desc orderBy

--- src/main/scala/gettingStarted/MnMcount.scala
+++ src/main/scala/gettingStarted/MnMcount.scala
@@ -1,7 +1,7 @@
 package gettingStarted
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, desc}
+import org.apache.spark.sql.functions.{col, count, desc}
 
 object MnMcount extends App {
 
@@ -12,7 +12,7 @@ object MnMcount extends App {
     .getOrCreate()
 
   // first 10 rows of csv
-  val mnm = spark.read
+  val mnmDF = spark.read
     .option("header", true)
     .option("inferSchema", true)
     .csv("src/main/scala/resources/chapter2/mnm_dataset.csv")
@@ -23,8 +23,16 @@ object MnMcount extends App {
     .text("src/main/scala/resources/chapter2/README.md")
 
   // the first 10 rows of a text
-  mnm.show(10, false)
-
+//  mnmDF.show(10, false)
+  //Aggregate counts of all colors and groupBy() State and Color orderBy() in descending order
+  val countMnMDF = mnmDF
+  .select("State", "Color", "Count")
+  .groupBy("State", "Color")
+  .agg(count("Count").alias("Total"))
+  .orderBy(desc("Total"))
+  // Show the aggregate results for all states and colors
+  countMnMDF.show(60)
+  println(s"Total Rows = ${countMnMDF.count()}")
 
 
 }
