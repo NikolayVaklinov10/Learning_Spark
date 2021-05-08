016ec143942bca08dee4368c7a9201831a914df9
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-08 14:26:58 +0100

    sorting the flights based on their delay time if any

--- src/main/scala/builtinDataSources/BasicQuery.scala
+++ src/main/scala/builtinDataSources/BasicQuery.scala
@@ -30,13 +30,27 @@ object BasicQuery extends App {
 //      |""".stripMargin).show(10)
 
   // Find all flights with more than 2 hour delay between Chicago(ORD) and San Francisco (SFO)
+//  spark.sql(
+//    """
+//      |SELECT date, delay, origin, destination
+//      |FROM us_delay_flights_tbl
+//      |WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
+//      |ORDER BY delay DESC
+//      |""".stripMargin).show(10)
+
+  // labeling all the flights based on the duration of the delay
   spark.sql(
     """
-      |SELECT date, delay, origin, destination
+      |SELECT delay, origin, destination,
+      |CASE
+      |     WHEN delay > 360 THEN 'Very Long Delays'
+      |     WHEN delay > 120 AND delay < 360 THEN 'Long Delays'
+      |     WHEN delay > 60 AND delay < 120 THEN 'Short Delays'
+      |     WHEN delay > 0 AND delay < 60 THEN 'Tolerable Delays'
+      |     ELSE 'Early'
+      |END AS Flight_Delays
       |FROM us_delay_flights_tbl
-      |WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
-      |ORDER BY delay DESC
-      |""".stripMargin).show(10)
-
+      |ORDER BY origin, delay DESC
+      |""".stripMargin).show(100)
 
 }
