69ed88ef8e83de15fdbeacce839a18768e5f69c0
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-09 15:44:51 +0100

    setting the path

---
+++ src/main/scala/externalDataSources/Flights.scala
@@ -0,0 +1,11 @@
+package externalDataSources
+
+object Flights extends App {
+
+  // Set file paths
+  val delaysPath = "src/main/scala/resources/chapter2/flights/departuredelays.csv"
+  val airportPath = "src/main/scala/resources/chapter2/flights/airport-codes-na.txt"
+
+
+
+}
