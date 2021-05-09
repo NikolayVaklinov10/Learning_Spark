10494382d034a6d78e67b799ddcbf3acb1f44135
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-09 15:47:32 +0100

    the spark session bit

--- src/main/scala/externalDataSources/Flights.scala
+++ src/main/scala/externalDataSources/Flights.scala
@@ -1,11 +1,19 @@
 package externalDataSources
 
+import org.apache.spark.sql.SparkSession
+
 object Flights extends App {
 
   // Set file paths
   val delaysPath = "src/main/scala/resources/chapter2/flights/departuredelays.csv"
   val airportPath = "src/main/scala/resources/chapter2/flights/airport-codes-na.txt"
 
+
+  // the spark session bit
+  val spark = SparkSession.builder()
+    .master("local[*]")
+    .appName("FlightsApp")
+    .getOrCreate()
 
 
 }
