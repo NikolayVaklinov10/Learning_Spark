af1ad35aaa8c949ca240bd1c176feb2d220667ad
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-09 21:10:59 +0100

    the spark session bit

--- src/main/scala/DatasetsAPI/Datasets.scala
+++ src/main/scala/DatasetsAPI/Datasets.scala
@@ -1,9 +1,17 @@
 package DatasetsAPI
 
+import org.apache.spark.sql.SparkSession
+
 object Datasets extends App {
 
   // making the case class needed for the schema of the dataset
   case class Bloggers(id:Int, first:String, last:String, url:String, date:String, hits: Int, campaigns:Array[String])
+
+  // the usual spark session
+  val spark = SparkSession.builder()
+    .appName("BloggerApp")
+    .master("local[*]")
+    .getOrCreate()
 
 
   // initializing a value for the blogger
