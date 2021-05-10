ab51d85c3991c20f36d832e84128bc7ef6bbfb6d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-10 08:10:53 +0100

    the generated dataset data

--- src/main/scala/DatasetsAPI/Datasets.scala
+++ src/main/scala/DatasetsAPI/Datasets.scala
@@ -18,12 +18,28 @@ object Datasets extends App {
   val bloggers = "src/main/scala/resources/chapter2/blogs.json"
 
   // the implicits needed
-  import spark.implicits._
+//  import spark.implicits._
   // making the dataset
   val bloggersDS = spark.read
     .format("json")
     .option("path", bloggers)
-    .load().as[Bloggers]
+//    .load().as[Bloggers]
+
+
+  // Usage dataset generated data
+  import scala.util.Random._
+  // the case class for the dataset
+  case class Usage(uid: Int, uname: String, usage: Int)
+  val r = new scala.util.Random(42)
+  // Create 1000 instances of scala Usage class
+  // This generates data on the fly
+  val data = for (i <- 0 to 1000)
+    yield (Usage(i, "user-" + r.alphanumeric.take(5).mkString(""),
+      r.nextInt(1000)))
+  // Create a Dataset of Usage typed data
+  import spark.implicits._
+  val dsUsage = spark.createDataset(data)
+  dsUsage.show(10)
 
 
 
