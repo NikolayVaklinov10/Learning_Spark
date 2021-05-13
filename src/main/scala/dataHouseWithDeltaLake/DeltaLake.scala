94dbcf0271163d47437d0a5de15659e47da1e1f4
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-13 14:53:57 +0100

    the path to the parquet file

---
+++ src/main/scala/dataHouseWithDeltaLake/DeltaLake.scala
@@ -0,0 +1,8 @@
+package dataHouseWithDeltaLake
+
+object DeltaLake extends App {
+
+  // Configure source data
+  val sourcePath = "src/main/scala/resources/chapter2/loans/loan-risks.snappy.parquet"
+
+}
