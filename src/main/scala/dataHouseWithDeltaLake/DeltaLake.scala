9fe823ea1f02b426c82373f651562cc1d08b7f08
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-13 14:55:34 +0100

    the path to the temporary folder

--- src/main/scala/dataHouseWithDeltaLake/DeltaLake.scala
+++ src/main/scala/dataHouseWithDeltaLake/DeltaLake.scala
@@ -5,4 +5,9 @@ object DeltaLake extends App {
   // Configure source data
   val sourcePath = "src/main/scala/resources/chapter2/loans/loan-risks.snappy.parquet"
 
+  // Configure Delta Lake path
+  val deltaPath = "/tmp/loans_data"
+
+
+
 }
