d66501699d59f1a703e28769c582fc769cc1c295
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-14 13:09:10 +0100

    the pipeline which is more convenient way to building the model

--- src/main/scala/sparkML/MachineLearning.scala
+++ src/main/scala/sparkML/MachineLearning.scala
@@ -1,5 +1,6 @@
 package sparkML
 
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.LinearRegression
@@ -38,6 +39,17 @@ object MachineLearning extends App {
     .setFeaturesCol("features")
     .setLabelCol("price")
   val lrModel = lr.fit(vecTrainDF)
+
+  // Inspecting the parameters learned from the model
+  val m = lrModel.coefficients(0)
+  val b = lrModel.intercept
+  println("*************************************************************************************")
+  println(f"""The formula for the linear regression line is price = $m%1.2f*bedrooms + $b%1.2f""")
+  println("*************************************************************************************")
+
+  // Building the Pipeline
+  val pipeline = new Pipeline().setStages(Array(vecAssembler, lr))
+  val pipelineModel = pipeline.fit(trainDF)
 
 
 
