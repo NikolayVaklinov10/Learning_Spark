3d8dbbc10ea2b017253d55ab436fae8b8ad32370
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-17 20:54:31 +0100

    the model for Alternating Least Squares algorithm, node the trainData needs fixing

--- src/main/scala/sparkML/Recommendations.scala
+++ src/main/scala/sparkML/Recommendations.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.ml.recommendation.{ALS, ALSModel}
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession, functions}
 import org.apache.spark.sql.functions._
 
 object Recommendations {
@@ -72,6 +72,30 @@ object Recommendations {
     artistAlias.head
 
     artistByID.filter($"id" isin(1208690, 1003926)).show()
+
+    def buildCounts(rawUserArtistData: Dataset[String], bArtistAlias: Broadcast[Map[Int,Int]]): DataFrame = {
+      rawUserArtistData.map { line =>
+        val Array(userID, artistID, count) = line.split(' ').map(_.toInt)
+        val finalArtistID =
+          bArtistAlias.value.getOrElse(artistID, artistID)
+          (userID, finalArtistID, count)
+      }.toDF("user", "artist", "count")
+    }
+
+
+    // The ALS Model
+    val model = new ALS().
+      setSeed(Random.nextLong()).
+      setImplicitPrefs(true).
+      setRank(10).
+      setRegParam(0.01).
+      setAlpha(1.0).
+      setMaxIter(5).
+      setUserCol("user").
+      setItemCol("artist").
+      setRatingCol("count").
+      setPredictionCol("prediction").
+      fit(trainData)
 
 
 
