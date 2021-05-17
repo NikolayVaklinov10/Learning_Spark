c40c573e7d22943c5e7e0554f82cf9d46c40dd85
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-17 19:10:57 +0100

    this part of the code will fail with NullPointerException because of map

--- src/main/scala/sparkML/Recommendations.scala
+++ src/main/scala/sparkML/Recommendations.scala
@@ -1,29 +1,41 @@
 package sparkML
 
-import org.apache.spark.sql.SparkSession
+import scala.collection.Map
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.ml.recommendation.{ALS, ALSModel}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions._
 
 object Recommendations {
+
 
   def main(args: Array[String]): Unit = {
 
     // the usual spark session
-    val spark  = SparkSession.builder()
+    val spark = SparkSession.builder()
       .master("local[*]")
       .appName("Audio Recommendation ML App")
       .getOrCreate()
 
     val rawUserArtistData = "src/main/scala/resources/chapter2/profiledata_06-May-2005/user_artist_data.txt"
 
-    val rawUserArtistDataDF = spark.read.text(rawUserArtistData)
+    val rawUserArtistDataDF = spark.read.textFile(rawUserArtistData)
 
-    rawUserArtistDataDF.take(5).foreach(println)
+    //    rawUserArtistDataDF.take(5).foreach(println)
+
+
+    // Note the following will fail
+    import spark.implicits._
+    rawUserArtistDataDF.map { line =>
+      val (id, name) = line.span(_ != '\t')
+      (id.toInt, name.trim)
+    }.count()
 
 
 
 
 
   }
-
-
-
 }
