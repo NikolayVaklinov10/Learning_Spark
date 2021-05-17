7d3a8e7f4f1f1516e90327f5f786fa7641f5e78f
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-17 17:21:58 +0100

    data exploration of the user_artist_data

--- src/main/scala/sparkML/Recommendations.scala
+++ src/main/scala/sparkML/Recommendations.scala
@@ -12,6 +12,12 @@ object Recommendations {
       .appName("Audio Recommendation ML App")
       .getOrCreate()
 
+    val rawUserArtistData = "src/main/scala/resources/chapter2/profiledata_06-May-2005/user_artist_data.txt"
+
+    val rawUserArtistDataDF = spark.read.text(rawUserArtistData)
+
+    rawUserArtistDataDF.take(5).foreach(println)
+
 
 
 
