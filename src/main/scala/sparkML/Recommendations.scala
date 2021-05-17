5ac1b27d34a9094d2c7bfd1a9c8e91186fa4aa8d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-17 17:10:26 +0100

    a little bit refactoring in order to have a main method for a particular entry point

--- src/main/scala/sparkML/Recommendations.scala
+++ src/main/scala/sparkML/Recommendations.scala
@@ -2,13 +2,22 @@ package sparkML
 
 import org.apache.spark.sql.SparkSession
 
-object Recommendations extends App {
+object Recommendations {
 
-  // the usual spark session
-  val spark  = SparkSession.builder()
-    .master("local[*]")
-    .appName("Audio Recommendation ML App")
-    .getOrCreate()
+  def main(args: Array[String]): Unit = {
+
+    // the usual spark session
+    val spark  = SparkSession.builder()
+      .master("local[*]")
+      .appName("Audio Recommendation ML App")
+      .getOrCreate()
+
+
+
+
+
+  }
+
 
 
 }
