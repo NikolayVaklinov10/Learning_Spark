290c249d0e6a59a8b23c7834957bbc1d8461e5e8
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-19 11:56:05 +0100

    enhancing the data frame

--- src/main/scala/sparkML/RandomForest.scala
+++ src/main/scala/sparkML/RandomForest.scala
@@ -11,6 +11,32 @@ object RandomForest {
       .appName("Random Forest Algorithm")
       .getOrCreate()
 
+    import spark.implicits._
+
+    val dataWithoutHeader = spark.read
+      .option("inferSchema", true)
+      .option("header", false)
+      .csv("src/main/scala/resources/chapter2/covtype.data")
+
+
+    val colNames = Seq(
+      "Elevation", "Aspect", "Slope",
+      "Horizontal_Distance_To_Hydrology", "Vertical_Distance_To_Hydrology",
+      "Horizontal_Distance_To_Roadways",
+      "Hillshade_9am", "Hillshade_Noon", "Hillshade_3pm",
+      "Horizontal_Distance_To_Fire_Points"
+    ) ++ (
+      (0 until 4).map(i => s"Wilderness_Area_$i")
+      ) ++ (
+      (0 until 40).map(i => s"Soil_Type_$i")
+      ) ++ Seq("Cover_Type")
+
+    val data = dataWithoutHeader.toDF(colNames:_*).
+      withColumn("Cover_Type", $"Cover_Type".cast("double"))
+
+    data.show()
+    data.head
+
   }
 
 }
